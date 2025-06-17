6ac9dd367e2aed642fafd083623ee32dad0e113a
ms-omori <omori.yasumitsu@makeshop.co.jp>
2025-06-17 16:50:03 +0900

    feat: 使えるモデルを整理する

--- src/limits.ts
+++ src/limits.ts
@@ -7,9 +7,41 @@ export class TokenLimits {
   constructor(model = 'gpt-3.5-turbo') {
     this.knowledgeCutOff = '2021-09-01'
     switch (model) {
+      case 'o4-mini':
+        this.maxTokens = 200000
+        this.responseTokens = 100000
+        this.knowledgeCutOff = '2024-06-01'
+        break
+      case 'o3':
+        this.maxTokens = 200000
+        this.responseTokens = 100000
+        this.knowledgeCutOff = '2024-06-01'
+        break
+      case 'o3-mini':
+        this.maxTokens = 200000
+        this.responseTokens = 100000
+        this.knowledgeCutOff = '2023-10-01'
+        break
+      case 'o1':
+        this.maxTokens = 200000
+        this.responseTokens = 100000
+        this.knowledgeCutOff = '2023-10-01'
+        break
+      case 'gpt-4.1':
+      case 'gpt-4.1-mini':
+      case 'gpt-4.1-nano':
+        this.maxTokens = 1047576
+        this.responseTokens = 32768
+        this.knowledgeCutOff = '2024-06-01'
+        break
       case 'gpt-4o':
         this.maxTokens = 128000
-        this.responseTokens = 4000
+        this.responseTokens = 16384
+        this.knowledgeCutOff = '2023-10-01'
+        break
+      case 'gpt-4o-mini':
+        this.maxTokens = 128000
+        this.responseTokens = 16384
         this.knowledgeCutOff = '2023-10-01'
         break
       case 'gpt-4-turbo':
@@ -17,22 +49,9 @@ export class TokenLimits {
         this.responseTokens = 4000
         this.knowledgeCutOff = '2023-12-01'
         break
-      case 'gpt-4-1106-preview':
-        this.maxTokens = 128000
-        this.responseTokens = 4000
-        this.knowledgeCutOff = '2023-04-01'
-        break
       case 'gpt-4':
         this.maxTokens = 8000
         this.responseTokens = 2000
-        break
-      case 'gpt-4-32k':
-        this.maxTokens = 32600
-        this.responseTokens = 4000
-        break
-      case 'gpt-3.5-turbo-16k':
-        this.maxTokens = 16300
-        this.responseTokens = 3000
         break
       default:
         this.maxTokens = 4000
